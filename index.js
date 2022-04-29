601b10c3c752f902cdc040f71ac005f3f3fd2082
Ajelandro19 <almon190699@gmail.com>
2022-04-29 14:40:00 -0500

    Paso 11: Crear endpoint final y aplicar ESlint

--- index.js
+++
@@ -1,6 +0,0 @@
-
-const ExplorerController=require("./lib/controllers/ExplorerController")
-
-console.log(ExplorerController.getExplorersByMission("node"));
-console.log(ExplorerController.getExplorersUsernamesByMission("node"));
-console.log(ExplorerController.getExplorersAmonutByMission("node"));
