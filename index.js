3c823c00130c68f443b1980d941ef7cef8a79fb5
Ajelandro19 <almon190699@gmail.com>
2022-04-29 14:09:33 -0500

    Paso 8: Crear ExplorerController y su prueba

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const Reader = require("./lib/utils/Reader");
-const ExplorerService = require("./lib/services/ExplorerServices");
-const explorers= Reader.readJsonFlie("explorers.json");
-const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
-/* console.log(explorers) */
+const ExplorerController=require("./lib/controllers/ExplorerController")
 
-// Aplicación del ExplorerService sobre la lista de explorers
-ExplorerService.filterByMission(explorers, "node");
-ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-ExplorerService.getExplorersUsernamesByMission(explorers, "node");
-const explorer1 = {name: "Explorer1", score: 1}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // {name: "Explorer1", score: 1, trick: 1} 
-
-const explorer3 = {name: "Explorer3", score: 3}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-
-const explorer5 = {name: "Explorer5", score: 5}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
-
-const explorer15 = {name: "Explorer15", score: 15}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+console.log(ExplorerController.getExplorersByMission("node"));
+console.log(ExplorerController.getExplorersUsernamesByMission("node"));
+console.log(ExplorerController.getExplorersAmonutByMission("node"));
---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,12 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Tests para ExplorerController",()=>{
+    test("Requerimiento 1: Calcular todos los explorers en una misión", ()=>{
+        const explorersInNode=ExplorerController.getExplorersByMission("node");
+        expect(explorersInNode.length).toBe(10);
+    });
+    test("Requerimiento 2: Obtener el username del explorer en una misión",()=>{
+        const explorersInNode=ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersInNode[0]).toBe("ajolonauta1");
+    });
+});
